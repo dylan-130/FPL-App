6d676f751baf45e165256166c85c074f8a8ec5ef
Dylan Byrne <dylan8byrne@gmail.com>
2025-02-10 12:51:52 +0000

    Update Navbar.tsx

--- Navbar.tsx
+++ Navbar.tsx
@@ -2,24 +2,26 @@ import { useState } from 'react';
 import './Navbar.css';
 
 const Navbar = () => {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    const toggleMenu = () => {
-        setIsMenuOpen(!isMenuOpen);
-    };
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
-    return (
-        <nav className="navbar">
-            <div className="navbar-logo">FPL Pulse</div>
-            <div className="hamburger-menu" onClick={toggleMenu}>
-                ☰
-            </div>
-            <div className={`navbar-links ${isMenuOpen ? 'show' : 'hide'}`}>
-                <a href="/">Home</a>
-                <a href="/connect">Connect</a>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">FPL Pulse</div>
+      {/* Hamburger Menu (Visible on Mobile) */}
+      <div className="hamburger-menu" onClick={toggleMenu}>
+        ☰
+      </div>
+      {/* Links: Full Navbar on Desktop / Responsive Toggle for Mobile */}
+      <div className={`navbar-links ${isMenuOpen ? 'show' : 'hide'}`}>
+        <a href="/">Home</a>
+        <a href="/connect">Connect</a>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
